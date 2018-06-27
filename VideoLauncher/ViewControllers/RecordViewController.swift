7403f60fde80fa41e6b038a501c527c426099a4a
Thomas Minshull <thomasminshull@gmail.com>
2018-06-26 17:27:54 -0700

    Added RecordViewController, and organized Project into Groups

---
+++ VideoLauncher/ViewControllers/RecordViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RecordViewController.swift
+//  VideoLauncher
+//
+//  Created by thomas minshull on 2018-06-26.
+//  Copyright © 2018 thomas minshull. All rights reserved.
+//
+
+import UIKit
+
+class RecordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func RecordButtonTapped(_ sender: UIButton) {
+    }
+    
+
+
+
+}
