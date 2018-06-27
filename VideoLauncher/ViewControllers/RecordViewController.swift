229f7ea05bfb6f61f0fa24345c0019b798f9b010
Thomas Minshull <thomasminshull@gmail.com>
2018-06-27 08:57:53 -0700

    Add Enum to manage state for RecordViewController

--- VideoLauncher/ViewControllers/RecordViewController.swift
+++ VideoLauncher/ViewControllers/RecordViewController.swift
@@ -9,17 +9,152 @@
 import UIKit
 
 class RecordViewController: UIViewController {
+    @IBOutlet var recordButton: UIButton!
+    
     let imagePickerController = UIImagePickerController()
     var image: UIImage?
     
+    private enum RecordState {
+        case recordPhoto
+        case recordVideo
+        case saveResults
+        case savingResults
+        case saved
+    }
+    
+    private var state: RecordState! {
+        didSet {
+            switch (oldValue, state) {
+            // setting intial value should be allowed
+            case (nil, .recordPhoto?):
+                setViewsForRecordPhtoto()
+                
+            // normal state transition
+            case (.recordPhoto?, .recordVideo?):
+                setViewsForRecordVideo()
+            case (.recordVideo?, .saveResults?):
+                setViewsForSave()
+            case (.saveResults?, .savingResults?):
+                setViewsForSaving()
+            case (.savingResults?, .saved?):
+                setViewsForSaved()
+            case (.saved?, .recordPhoto?):
+                setViewsForRecordPhtoto()
+                
+            // canceled state transition
+            case (_, .recordPhoto?):
+                setViewsForRecordPhtoto()
+                
+            // invalid state transitions
+            case (_, nil):
+                fallthrough
+            case (.recordPhoto?, .savingResults?):
+                fallthrough
+            case (.recordPhoto?, .saved?):
+                fallthrough
+            default:
+                print("Invalid state transfer in RecordViewController attemted, resetting state to previous value")
+                state = oldValue
+            }
+        }
+    }
+    
+    // MARK: - Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        state = .recordPhoto // set initial state
         navigationController?.setNavigationBarHidden(true, animated: false)
         imagePickerController.delegate = self
         UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
+    // MARK: - State Transitions
+    
+    private func cancel() {
+        guard state != .saved else {
+            print("Cannot Cancel from Saved State")
+            return
+        }
+        state = .recordPhoto
+    }
+    
+    // Normal State Transitions
+    private func transitionToNextState() {
+        switch state {
+        case nil:
+            state = .recordPhoto
+        case .recordPhoto?:
+            state = .recordVideo
+        case .recordVideo?:
+            state = .saveResults
+        case .saveResults?:
+            state = .savingResults
+        case .savingResults?:
+            state = .saved
+        case .saved?:
+            state = .recordPhoto
+        }
+    }
+    
+    // MARK: - set Views for state
+    
+    private func setViewsForRecordPhtoto() {
+        DispatchQueue.main.async {
+            self.recordButton.isEnabled = true
+            self.recordButton.tintColor = UIColor.red
+            self.recordButton.setTitle("Record Picture", for: .normal)
+        }
+    }
+    
+    private func setViewsForRecordVideo() {
+        DispatchQueue.main.async {
+            self.recordButton.isEnabled = true
+            self.recordButton.tintColor = UIColor.red
+            self.recordButton.setTitle("Record Video", for: .normal)
+        }
+    }
+
+    private func setViewsForSave() {
+        DispatchQueue.main.async {
+            self.recordButton.isEnabled = true
+            self.recordButton.tintColor = UIColor.blue
+            self.recordButton.setTitle("Save", for: .normal)
+        }
+    }
+    
+    private func setViewsForSaving() {
+        DispatchQueue.main.async {
+            self.recordButton.isEnabled = false
+            self.recordButton.tintColor = UIColor.blue
+            self.recordButton.setTitle("Saving", for: .normal)
+        }
+    }
+    
+    private func setViewsForSaved() {
+        DispatchQueue.main.async {
+            self.recordButton.isEnabled = false
+            self.recordButton.tintColor = UIColor.blue
+            self.recordButton.setTitle("Saved", for: .normal)
+        }
+    }
+    
+    // MARK: - Actions
+    
     @IBAction func RecordButtonTapped(_ sender: UIButton) {
+        // change buttons behavour based on state // refactore each behavour into different functions
+        switch state {
+        case .recordPhoto?:
+            presentCamerForPhoto()
+        default:
+            return
+        }
+        
+    }
+    
+    // MARK: - Helper Functions
+    
+    func presentCamerForPhoto() {
         let imageKey = "public.image"
         
         guard (UIImagePickerController.availableMediaTypes(for: .camera)?.contains(imageKey))! else {
@@ -31,18 +166,38 @@ class RecordViewController: UIViewController {
         imagePickerController.allowsEditing = true
         present(imagePickerController, animated: true, completion: nil)
     }
+    
 }
 
 extension RecordViewController: UIImagePickerControllerDelegate {
     
     public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // ToDo possibly update state here based on previous state
         
+        switch state {
+        case .recordPhoto?:
+            handleDidFetchPhotoFrom(picker: picker, With: info)
+           
+        case .recordVideo?:
+        // ToDo: handleFetchingVideo
+            break
+        default:
+            return
+        }
+    }
+    
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        cancel()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func handleDidFetchPhotoFrom(picker: UIImagePickerController, With info: [UIImagePickerController.InfoKey : Any]) {
         if let editedImage = info[.editedImage] as? UIImage {
             image = editedImage
         } else {
             image = info[.originalImage] as? UIImage
         }
-
+        
         let imageTesterVC = storyboard?.instantiateViewController(withIdentifier: "ARImageTesterViewController") as! ARImageTesterViewController
         imageTesterVC.image = image
         imageTesterVC.delegate = self
@@ -51,46 +206,43 @@ extension RecordViewController: UIImagePickerControllerDelegate {
         present(imageTesterVC, animated: true, completion: nil)
     }
     
-    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
+    private func handleDidFetchVideoFrom(picker: UIImagePickerController, With info: [UIImagePickerController.InfoKey : Any]) {
+//        // ToDo Change implementation based on state
+//        if let editedImage = info[.editedImage] as? UIImage {
+//            image = editedImage
+//        } else {
+//            image = info[.originalImage] as? UIImage
+//        }
+//
+//        let imageTesterVC = storyboard?.instantiateViewController(withIdentifier: "ARImageTesterViewController") as! ARImageTesterViewController
+//        imageTesterVC.image = image
+//        imageTesterVC.delegate = self
+//
+//        picker.dismiss(animated: false)
+//        present(imageTesterVC, animated: true, completion: nil)
     }
 }
 
 extension RecordViewController: ImageTesterDelegate {
     func ARImageTesterViewController(_ imageTesterViewController: ARImageTesterViewController, didVerify image: UIImage) {
         imageTesterViewController.dismiss(animated: true, completion: nil)
+      
+        transitionToNextState()
         
-        // We have an image 
+        // ToDO update state
         
+        // We have an image
+        // ToDo:
+        /*
+         - get video
+         - create dataStructure to store Image / video's
+         - persist Image/Video
+         - load Image/Video
+         - Refactor, clean up code
+         - Implement Anchoring on longpress from other branch
+         */
     }
-    
-    
 }
 
 extension RecordViewController: UINavigationControllerDelegate {
-//    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
-//
-//    }
-//
-//    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
-//
-//    }
-//
-//
-//    public func navigationControllerSupportedInterfaceOrientations(_ navigationController: UINavigationController) -> UIInterfaceOrientationMask {
-//
-//    }
-//
-//    public func navigationControllerPreferredInterfaceOrientationForPresentation(_ navigationController: UINavigationController) -> UIInterfaceOrientation {
-//
-//    }
-//
-//    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
-//
-//    }
-//
-//    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//
-//    }
-
 }
