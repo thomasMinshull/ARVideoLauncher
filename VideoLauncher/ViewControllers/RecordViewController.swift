72868868a5571f84a37ce56526a30b733125fe18
Thomas Minshull <thomasminshull@gmail.com>
2018-06-26 19:57:27 -0700

    Added UIImagePickerController and taking initial picture

--- VideoLauncher/ViewControllers/RecordViewController.swift
+++ VideoLauncher/ViewControllers/RecordViewController.swift
@@ -9,17 +9,73 @@
 import UIKit
 
 class RecordViewController: UIViewController {
-
+    let imagePickerController = UIImagePickerController()
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        imagePickerController.delegate = self
+        UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     @IBAction func RecordButtonTapped(_ sender: UIButton) {
+        let imageKey = "public.image"
+        
+        guard (UIImagePickerController.availableMediaTypes(for: .camera)?.contains(imageKey))! else {
+            return
+        }
+        
+        imagePickerController.mediaTypes = [imageKey]
+        imagePickerController.sourceType = UIImagePickerController.SourceType.camera
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true, completion: nil)
+    }
+}
+
+extension RecordViewController: UIImagePickerControllerDelegate {
+    
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let editedImage = info[.editedImage] as? UIImage {
+            image = editedImage
+        } else {
+            image = info[.originalImage] as? UIImage
+        }
+
+        // ToDo Segue to next scene to Test Image Detection 
     }
     
+    public func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
 
 
+extension RecordViewController: UINavigationControllerDelegate {
+//    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+//
+//    }
+//
+//    public func navigationController(_ navigationController: UINavigationController, didShow viewController: UIViewController, animated: Bool) {
+//
+//    }
+//
+//
+//    public func navigationControllerSupportedInterfaceOrientations(_ navigationController: UINavigationController) -> UIInterfaceOrientationMask {
+//
+//    }
+//
+//    public func navigationControllerPreferredInterfaceOrientationForPresentation(_ navigationController: UINavigationController) -> UIInterfaceOrientation {
+//
+//    }
+//
+//    public func navigationController(_ navigationController: UINavigationController, interactionControllerFor animationController: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
+//
+//    }
+//
+//    public func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+//
+//    }
 
 }
